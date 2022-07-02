bdf8aae2760cdfd1e29c7957680203033fb21c09
franciscommelo <48535341+franciscommelo@users.noreply.github.com>
2022-07-02 20:25:00 +0100

    fix cors filter

--- src/main/java/com/example/carmark/CarMarkApplication.java
+++ src/main/java/com/example/carmark/CarMarkApplication.java
@@ -22,6 +22,8 @@ public class CarMarkApplication {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://boot-hheroku.herokuapp.com"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://vehicle-manager-backend.herokuapp.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
